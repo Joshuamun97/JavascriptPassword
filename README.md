29528aaa0aa7ec406dc481f0be98544f62e82764
Joshua_Muniz <joshua.mun97@gmail.com>
2022-11-25 17:45:42 -0700

    Final Commit. Deleted additional .js, included phtos for read me, finalized javascript

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# JavascriptPassword
+# <Password Generator>
+
+## Description
+
+Have you ever been in need of a new secure, randomly generated password but just dont have ideas? What if you're having issues meeting specific password criterias for a new log in password? Well, This random password generator takes the guessing out of making a password? In this Challenge I used JavaScript to create an interactive password generator where you choose the criteria and in return you get a randomly generated password? During the duration of this project i've learned the importance of for loops, proper use of if/else, and a fun one for me was the math.floor!
+
+## Installation
+
+Below are the installation steps:
+1. Create your variables
+2. Decide on what functions are needed and write out your skeleton functions as placeholders to return to
+3. Begin diving into the function arguments.
+4. Make sure to create a function that actually runs the code you are trying to create. Otherwise nothing will execute!
+
+## Usage
+
+This can be used to create a random password with user specified criteria. You can choose the length, whether or not you want numbers, uppercase letters, lowercase letters, or special characters!
+
+
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.27%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.37%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.42%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.45%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.47%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.48%20PM.png)
+![alt text](assets/Screenshot%202022-11-25%20at%205.33.52%20PM.png)
+
+## Credits
+
+Justus Schoppman
+Rachel Wildberger
+Sergio Perkins
+
+## License
+
+N/A
+
+---
--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] ;
+var specialcharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'];
+
+function getPasswordCriteria() {
+  var userWantsLengthOf = parseInt(prompt("How many characters do you want in your password?"))
+   while(isNaN(userWantsLengthOf) || userWantsLengthOf < 8 || userWantsLengthOf > 126) {
+    userWantsLengthOf=prompt ("Character length needs to be a number, and range between 8-126 digits. Please make sure you are using a numerical charater and the proper range amount");
+   }
+   
+  var userChoseNumbers = confirm("Do you want numbers in your password?"); 
+  var userChoseUppercase = confirm ("Do you want Uppercase Letters in your password?");
+  var userChoseLowercase = confirm ("Do you want Lowercase Letters in your password?");
+  var userChoseSpecialCharacters = confirm ("Do you want Special Characters in your password?");
+  var options = {
+    userChoseNumbers: userChoseNumbers,
+    userChoseUppercase: userChoseUppercase, 
+    userChoseLowercase: userChoseLowercase,
+    userChoseSpecialCharacters: userChoseSpecialCharacters,
+    userWantsLengthOf: userWantsLengthOf,
+  };
+  return options;
+}
+function createRandom(length) {
+  var randomIndex = Math.floor(Math.random() * length);
+  return randomIndex;
+}
+
+function generatePassword() {
+    var userChose = getPasswordCriteria();
+    var availableChars = [];
+    var passwordArr = [];
+
+    if(userChose.userChoseNumbers) {
+      availableChars = availableChars.concat(numbers);
+    }
+    if(userChose.userChoseLowercase) {
+      availableChars = availableChars.concat(lowercase);
+    }
+    if(userChose.userChoseUppercase) {
+      availableChars = availableChars.concat(uppercase);
+      if(userChose.userChoseSpecialCharacters) {
+        availableChars = availableChars.concat(specialcharacters);
+      }
+    } 
+  
+    for(var i = 0; i < userChose.userWantsLengthOf; i++) {
+      passwordArr.push(availableChars[createRandom(availableChars.length)])
+    }
+
+
+  return passwordArr.join("")
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
